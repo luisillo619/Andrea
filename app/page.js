d5aea7f41bacc5a183ad7f826776d0e9ece83612
luisillo619 <luiscarlosrangel619@gmail.com>
2023-05-17 00:10:54 -0600

    andrae

--- app/page.js
+++ app/page.js
@@ -1,7 +1,26 @@
-import Image from 'next/image'
+"use client";
+import Aos from "aos";
+import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    Aos.init({
+      duration: 1500,
+    });
+  }, []);
   return (
-   <h1>Feliz cumpleaños</h1>
-  )
+    <div
+      data-aos="fade-in"
+      className="flex flex-col justify-center items-center w-full h-full"
+    >
+      <h1>Feliz cumpleaños, ANDREAAAAAAAAAA</h1>
+      <Image
+        src="https://th.bing.com/th/id/OIP.DXPQyG3w0a4NZ7J7h-CoBwHaHa?pid=ImgDet&rs=1"
+        width={200}
+        height={200}
+        className="object-contain"
+      />
+    </div>
+  );
 }
